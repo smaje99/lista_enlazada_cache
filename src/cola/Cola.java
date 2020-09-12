5d21cd9b48af67838c29cb77c321c5e46bccba3b
Sergio Majé <s.maje@udla.edu.co>
2020-09-12 03:42:32 -0500

    Create Cola

---
+++ src/cola/Cola.java
@@ -0,0 +1,58 @@
+package cola;
+
+import dato.Persona;
+
+public class Cola {
+    private int tope;
+    private final int limite;
+    private final Persona[] cola;
+
+    public Cola(int limite) {
+        cola = new Persona[limite];
+        tope = 0;
+        this.limite = limite;
+        for (int i = 0; i < limite; i++) cola[i] = null;
+    }
+
+    public boolean isEmpty() {
+        return tope == 0;
+    }
+
+    public boolean isFull() {
+        return tope == limite;
+    }
+
+    public boolean push(Persona dato) {
+        if (!isEmpty() && !isFull()) {
+            cola[tope++] = dato;
+            return true;
+        }
+        return false;
+    }
+
+    public Persona pop() {
+        if (!isEmpty()) {
+            Persona pop = cola[0];
+            for (int i = 0; i < limite - 1; i++) cola[i] = cola[i + 1];
+            tope--;
+            cola[tope] = null;
+            return pop;
+        }
+        return null;
+    }
+
+    public Persona search(String nombreCompleto) {
+        if (!isEmpty()) {
+            if (cola[0].equals(nombreCompleto))
+                return cola[0];
+            else if (tope > 1 && cola[tope - 1].equals(nombreCompleto))
+                return cola[tope - 1];
+            else {
+                for (int i = 1; i < tope - 1; i++)
+                    if (cola[i].equals(nombreCompleto))
+                        return cola[i];
+            }
+        }
+        return null;
+    }
+}
